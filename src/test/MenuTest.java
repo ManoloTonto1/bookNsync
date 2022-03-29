e16f9fd96610f294bebf085fa5414b9ae2eea3a4
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2022-03-29 23:05:17 +0200

    Update MenuTest.java

--- src/test/MenuTest.java
+++ src/test/MenuTest.java
@@ -20,7 +20,7 @@ public class MenuTest {
         assertTrue(condition);
     }
     @Test
-    public void Login_checkpasscorrect_loginNotsuccessfull(){
+    public void Login_checkpassincorrect_loginNotsuccessfull(){
         //arrange
         Settings settings = Settings.getInstanceTest();
         String input = "bomama123";
